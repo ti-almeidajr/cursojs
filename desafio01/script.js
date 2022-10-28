dd7d43f353f9ef91a96c808ac86ade5c4b16da06
Almeida Júnior <114241904+tic-almeidajr@users.noreply.github.com>
2022-10-28 13:23:36 -0300

    desafio js

---
+++ desafio01/script.js
@@ -0,0 +1,8 @@
+//Elementos
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+
+function adicionar()
